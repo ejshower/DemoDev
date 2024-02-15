61159912e858331f778a0cbbcee4ce402bdcb6f7
ejshower <ejshower@gmail.com>
2024-02-15 21:13:11 +0900

    first git upload

---
+++ CollectionDemo/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CollectionDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private Hashtable GetHashtable() 
+        {
+            Hashtable ht= new Hashtable();
+            ht.Add(1, "Good");
+            ht.Add(2, "Morning");
+            ht.Add(3, "Afternoon");
+            ht.Add(4, "Evening");
+            //ht.Add(1, "so so");
+            return ht;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string strFirst = string.Empty;
+
+            StringBuilder sb = new StringBuilder();
+            Hashtable     ht = GetHashtable();
+
+            foreach (int key in ht.Keys)
+            {
+                if (ht.ContainsKey(key))
+                {
+                    if(key == 1)
+                    {
+                        strFirst = ht[key] as string;
+                        continue;
+                    }
+                    sb.AppendLine(strFirst +" "+ ht[key] as string);
+                }
+            }
+
+            rtbGreeting.Text = sb.ToString();
+        }
+    }
+}
